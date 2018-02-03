7f81769030d4af04546a815caadd23c01b0511b8
Rajat sharma <rajatsharma369007@gmail.com>
2018-02-03 14:23:16 +0530

    Add files via upload

---
+++ Code/Smart_plug.ino
@@ -0,0 +1,50 @@
+
+
+#include <SoftwareSerial.h>  
+
+int bluetoothTx = 2;
+int bluetoothRx = 3;
+
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+
+int switch = 13;
+
+char cmd[100];
+int cmdIndex;
+
+
+void exeCmd() {
+  
+  if(strcmp(cmd, "off")==0) digitalWrite(led, LOW);
+  if(strcmp(cmd, "on")==0) digitalWrite(led, HIGH);
+}
+
+
+void setup() {
+  
+  bluetooth.begin(9600); // Bluetooth default baud is 115200
+ 
+  pinMode(switch, OUTPUT);
+  digitalWrite(switch, LOW);
+  
+  cmdIndex = 0;
+}
+
+
+void loop() {
+  
+  if(bluetooth.available()) {
+    
+    char c = (char)bluetooth.read();
+    
+    if(c=='\n') {
+      cmd[cmdIndex] = 0;
+      exeCmd();  // execute the command
+      cmdIndex = 0; // reset the cmdIndex
+    } else {      
+      cmd[cmdIndex] = c;
+      if(cmdIndex<99) cmdIndex++;
+    }
+  }
+}
+
