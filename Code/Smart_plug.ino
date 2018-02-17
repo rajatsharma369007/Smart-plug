5fcdc48c32e1760e6efd4284ffbc1924625a3fcc
Rajat sharma <rajatsharma369007@gmail.com>
2018-02-17 15:23:31 +0530

    Update Smart_plug.ino

--- Code/Smart_plug.ino
+++ Code/Smart_plug.ino
@@ -5,14 +5,18 @@
 int bluetoothTx = 2;
 int bluetoothRx = 3;
 
+//to have the serial communication pins as 2, 3 instead of 0,1 due to avrrude problem while uploading 
+//the program through ftdi bootloader
+
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
-int switch = 13;
+int switch = 13;      //relay connected at pin 13
 
-char cmd[100];
-int cmdIndex;
+char cmd[10];        //to store the on/off string
+int cmdIndex;     
 
 
+//function to trigger the relay
 void exeCmd() {
   
   if(strcmp(cmd, "off")==0) digitalWrite(led, LOW);
@@ -22,7 +26,7 @@ void exeCmd() {
 
 void setup() {
   
-  bluetooth.begin(9600); // Bluetooth default baud is 115200
+  bluetooth.begin(9600); // Bluetooth default baud is 115200 (important, you will not get any error but create problem)
  
   pinMode(switch, OUTPUT);
   digitalWrite(switch, LOW);
@@ -32,13 +36,15 @@ void setup() {
 
 
 void loop() {
-  
+
+  //waiting of the bluetooth to send the command
   if(bluetooth.available()) {
-    
+    //read the character sent
     char c = (char)bluetooth.read();
     
+    //storing the character in cmd[] string till it gets complete on/off string
     if(c=='\n') {
-      cmd[cmdIndex] = 0;
+      cmd[cmdIndex] = 0;  
       exeCmd();  // execute the command
       cmdIndex = 0; // reset the cmdIndex
     } else {      
